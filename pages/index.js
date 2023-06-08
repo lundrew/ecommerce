52b964f36aca6c25bf77093f2e74949be1b6917a
lundrew <92401835+lundrew@users.noreply.github.com>
2023-06-08 12:50:20 -0700

    search feature for products

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 import { client } from "../lib/client";
-import React from "react";
+import React, { useState } from "react";
 import { Product, FooterBanner, HeroBanner } from "../components";
 
 const Home = ({ products, bannerData }) => {
+  const [search, setSearch] = useState("");
+
+  const filteredProd = products.filter((prod) =>
+    prod.name.toLowerCase().includes(search.toLowerCase())
+  );
+  const handleInputChange = (e) => {
+    setSearch(e.target.value);
+  };
   return (
     <div>
       <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
@@ -10,13 +18,19 @@ const Home = ({ products, bannerData }) => {
         <h2>Best Selling</h2>
         <p></p>
       </div>
-
+      <div className="product-search">
+        <input
+          className="product-search-input"
+          value={search}
+          onChange={handleInputChange}
+          placeholder="Search"
+        ></input>
+      </div>
       <div className="products-container">
-        {products?.map((product) => (
+        {filteredProd?.map((product) => (
           <Product key={product._id} product={product} />
         ))}
       </div>
-
       <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </div>
   );
